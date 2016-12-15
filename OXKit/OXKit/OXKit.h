eca6366f05d15b14b03e39bce39c22a95c104a6d
Dmitriy Azarov <oxozle@gmail.com>
2016-12-15 17:29:58 +0300

    Create project

---
+++ OXKit/OXKit/OXKit.h
@@ -0,0 +1,19 @@
+//
+//  OXKit.h
+//  OXKit
+//
+//  Created by Dmitriy on 15/12/2016.
+//  Copyright © 2016 Oxozle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OXKit.
+FOUNDATION_EXPORT double OXKitVersionNumber;
+
+//! Project version string for OXKit.
+FOUNDATION_EXPORT const unsigned char OXKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OXKit/PublicHeader.h>
+
+

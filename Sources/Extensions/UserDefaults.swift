d55b61e17c05e7e6525929e084e1d42e9567d890
Dmitriy Azarov <oxozle@gmail.com>
2017-10-17 17:51:08 +0300

    Lock + Web View

--- Sources/Extensions/UserDefaults.swift
+++ Sources/Extensions/UserDefaults.swift
@@ -26,7 +26,21 @@
 
 import Foundation
 
+private let UserDefaultsLockPrefix = "LOCK_"
 public extension UserDefaults {
+    public class func isLocked(key: String) -> Bool {
+        return UserDefaults.standard.object(forKey: getLockKey(key)) != nil
+    }
+    
+    public class func lock(key: String) {
+        UserDefaults.standard.set(true, forKey: getLockKey(key))
+        UserDefaults.standard.synchronize()
+    }
+    
+    private class func getLockKey(_ key: String) -> String {
+        return "\(UserDefaultsLockPrefix)\(key.uppercased())"
+    }
+    
     public func bool(forKey defaultName: String, defaultValue: Bool) -> Bool {
         if UserDefaults.standard.object(forKey: defaultName) == nil {
             return defaultValue
--- Sources/UI/OXWebView.swift
+++ Sources/UI/OXWebView.swift
@@ -6,4 +6,92 @@
 //  Copyright © 2017 Oxozle. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import WebKit
+
+public class OXWebView: WKWebView {
+    public init(configuration: WKWebViewConfiguration) {
+        super.init(frame: CGRect.zero, configuration: configuration)
+        
+        let bundle = Bundle(for: type(of: self))
+        let source = try! String(contentsOf: bundle.url(forResource: "OXWebView", withExtension: "js")!, encoding: String.Encoding.utf8)
+        let userScript = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        configuration.userContentController.addUserScript(userScript)
+        configuration.userContentController.add(self, name: "oxkithandler")
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        scrollView.decelerationRate = UIScrollViewDecelerationRateNormal
+        
+        navigationDelegate = self
+        scrollView.bounces = true
+        scrollView.alwaysBounceVertical = true
+        uiDelegate = self
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    public func load(url: String) {
+        if let startUrl = URL(string: url) {
+            var request = URLRequest(url: startUrl)
+            load(request)
+        }
+    }
+}
+
+extension OXWebView: WKScriptMessageHandler {
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        
+    }
+}
+
+extension OXWebView: WKNavigationDelegate {
+    public func webView(_ webView: WKWebView,
+                 didFail navigation: WKNavigation,
+                 withError error: Error) {
+        print("webView:\(webView) didFailNavigation:\(navigation) withError:\(error)")
+    }
+    
+    public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+}
+
+extension OXWebView: WKUIDelegate {
+    public func webView(_ webView: WKWebView,
+                 runJavaScriptAlertPanelWithMessage message: String,
+                 initiatedByFrame frame: WKFrameInfo,
+                 completionHandler: @escaping () -> Void) {
+//        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+//        let ok = UIAlertAction(title: "OK", style: .default) { (_: UIAlertAction) -> Void in
+//            alert.dismiss(animated: true, completion: nil)
+//        }
+//        alert.addAction(ok)
+//        webViewDelegate?.webView(present: alert)
+        completionHandler()
+    }
+    
+    public func webView(_ webView: WKWebView,
+                 runJavaScriptConfirmPanelWithMessage message: String,
+                 initiatedByFrame frame: WKFrameInfo,
+                 completionHandler: @escaping (Bool) -> Void) {
+//        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { (_: UIAlertAction) -> Void in
+//            completionHandler(false)
+//        })
+//        alert.addAction(UIAlertAction(title: "OK", style: .default) { (_: UIAlertAction) -> Void in
+            completionHandler(true)
+//        })
+//        webViewDelegate?.webView(present: alert)
+    }
+    
+    @available(iOS 10.0, *)
+    public func webView(_ webView: WKWebView, shouldPreviewElement elementInfo: WKPreviewElementInfo) -> Bool {
+        return false
+    }
+}
+
